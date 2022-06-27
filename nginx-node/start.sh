794d7bc3222b8320fbb7a38268d4328f8f986a4a
/rdndev <rdninc@mail.ru>
2022-06-28 00:29:41 +0300

    add comments

--- nginx-node/start.sh
+++ nginx-node/start.sh
@@ -1,7 +1,8 @@
+# Logging
 ln -sf /proc/1/fd/1 /var/log/nginx/error.log
 ln -sf /proc/1/fd/1 /var/log/nginx/access.log
-
+# Set owner and group www-data to html directory
 chown www-data:www-data -R /var/www/html
-
+# Start services: nginx and php-fpm
 service php7.3-fpm start
 service nginx start
